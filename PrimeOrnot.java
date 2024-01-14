c278af51fa09b1ba071d2bde7a496a881e093224
Sunil Neupane <141554245+SunilNeupane77@users.noreply.github.com>
2024-01-14 07:07:02 +0545

    checking Prime number

---
+++ PrimeOrnot.java
@@ -0,0 +1,38 @@
+
+
+// java Program to find whether a Number
+// is Prime or Not using Recursion
+import java.util.*;
+
+public class PrimeOrnot {
+
+    // Returns true if n is prime, else
+    // return false.
+    // i is current divisor to check.
+    static boolean isPrime(int n, int i)
+    {
+
+        // Base cases
+        if (n <= 2)
+            return (n == 2) ? true : false;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+
+        // Check for next divisor
+        return isPrime(n, i + 1);
+    }
+
+    // Driver program to test above function
+    public static void main(String[] args)
+    {
+
+        int n = 15;
+
+        if (isPrime(n, 2))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
